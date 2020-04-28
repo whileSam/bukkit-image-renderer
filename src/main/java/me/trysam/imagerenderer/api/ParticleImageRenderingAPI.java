1673ce32241aa523e0b8f95cb9bbc1166047b733
Samuel <samuel.eichelmann@gmail.com>
2020-04-29 01:25:35 +0200

    [+] Added copyright notices

--- src/main/java/me/trysam/imagerenderer/api/ParticleImageRenderingAPI.java
+++ src/main/java/me/trysam/imagerenderer/api/ParticleImageRenderingAPI.java
@@ -1,18 +1,35 @@
+/*
+ * Bukkit Image Renderer - A simple plugin to display images in Minecraft via particles.
+ * Copyright (c) 2020. Samuel Eichelmann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
 package me.trysam.imagerenderer.api;
 
 import me.trysam.imagerenderer.math.Quaternion;
 import me.trysam.imagerenderer.math.Vec3d;
 import me.trysam.imagerenderer.math.Vec3f;
 import me.trysam.imagerenderer.particle.ImageRenderer;
-import me.trysam.imagerenderer.particle.ParticleRenderer;
 import me.trysam.imagerenderer.util.Axis;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collection;
 
 public class ParticleImageRenderingAPI {
@@ -21,9 +38,26 @@ public class ParticleImageRenderingAPI {
     private Vec3d location;
     private Quaternion rotation = new Quaternion(new Vec3f(1, 0, 0),0);
 
+    /**
+     *
+     * @param file The file of the image.
+     * @param location The location where the image is rendered Represented as a vector.
+     * @throws IOException when the image could not be found
+     */
     public ParticleImageRenderingAPI(File file, Vec3d location) throws IOException {
         this.image = ImageIO.read(file);
         this.location = location;
+    }
+
+    /**
+     *
+     * @param file The file of the image.
+     * @param location The location where the image is rendered. Represented as a location.
+     * @throws IOException when the image could not be found
+     */
+    public ParticleImageRenderingAPI(File file, Location location) throws IOException {
+        this.image = ImageIO.read(file);
+        this.location = Vec3d.fromLocation(location);
     }
 
 
--- src/main/java/me/trysam/imagerenderer/math/Vec3d.java
+++ src/main/java/me/trysam/imagerenderer/math/Vec3d.java
@@ -1,3 +1,21 @@
+/*
+ * Bukkit Image Renderer - A simple plugin to display images in Minecraft via particles.
+ * Copyright (c) 2020. Samuel Eichelmann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
 package me.trysam.imagerenderer.math;
 
 import org.bukkit.Location;
